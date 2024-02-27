2a8abfe03441268149a606d18b27a9230f82cb74
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-02-27 19:33:26 +0200

    add product description to details view

--- lib/views/details_view/widgets/details_view_body.dart
+++ lib/views/details_view/widgets/details_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../models/models/product_model.dart';
+import 'product_description.dart';
 import 'product_info.dart';
 
 class DetailsViewBody extends StatelessWidget {
@@ -13,9 +16,32 @@ class DetailsViewBody extends StatelessWidget {
     return SingleChildScrollView(
       child: SizedBox(
         width: double.infinity,
+        height: MediaQuery.sizeOf(context).height,
         child: Stack(
           children: [
             ProductInfo(product: product),
+            Positioned(
+              top: 310,
+              left: 0,
+              right: 0,
+              child: ProductDescription(
+                product: product,
+                onPressed: () {},
+              ),
+            ),
+            Positioned(
+              top: 45.h,
+              right: -30.w,
+              child: Hero(
+                tag: product.id,
+                child: Image.network(
+                  product.image,
+                  fit: BoxFit.contain,
+                  height: 300.h,
+                  width: 300.w,
+                ),
+              ),
+            ),
           ],
         ),
       ),
