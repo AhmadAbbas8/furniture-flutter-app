2640606538a99703296f970c32a8e2685960b344
AhmadAbbas8 <ahmadabbass822@gmail.com>
2024-02-27 14:43:42 +0200

    build appbar for home view

--- lib/core/utils/assets_manager.dart
+++ lib/core/utils/assets_manager.dart
@@ -1,4 +1,6 @@
 abstract class AssetsManager {
-  static const _assetsBase = 'assets';
-
+  static const _assetsBase = 'assets/';
+  static const _icons = '${_assetsBase}icons/';
+  static const scan = '${_icons}scan.svg';
+  static const menu = '${_icons}menu.svg';
 }
--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furniture_flutter_app/core/utils/assets_manager.dart';
+import 'package:furniture_flutter_app/core/utils/colors_palette.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,7 +10,36 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          AssetsManager.menu,
+          height: 20.h,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            AssetsManager.scan,
+            height: 24.h, //24
+          ),
+          onPressed: () {},
+        ),
+        const Text(
+          "Scan",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorsPalette.kTextColor,
+          ),
+        ),
+        SizedBox(width: 20.w),
+      ],
     );
   }
 }
